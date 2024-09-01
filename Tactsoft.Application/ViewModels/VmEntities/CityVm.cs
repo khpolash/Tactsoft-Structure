fa345f07c55cb4a2c8808b64ab1c691dc54a11e6
khpolash <khpolashbd@gmail.com>
2024-09-01 12:14:39 +0600

    Enhance CityVm with Validation Attributes
    - Added Data Annotations for validation on CityVm properties:
      - [Required] attribute with custom error messages for Name properties.
      - [StringLength] attribute with custom error messages for Name properties.
    
    This update ensures that the CityVm class enforces validation rules and provides meaningful error messages for invalid input.

--- Tactsoft.Application/ViewModels/VmEntities/CityVm.cs
+++ Tactsoft.Application/ViewModels/VmEntities/CityVm.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Tactsoft.SharedKernel.Entities.BaseEntities;
 
@@ -6,6 +7,8 @@ namespace Tactsoft.Application.ViewModels.VmEntities;
 
 public class CityVm : BaseEntity
 {
+    [Required(ErrorMessage = "The City name is required.")]
+    [StringLength(85, ErrorMessage = "The City name cannot exceed 85 characters.")]
     public string Name { get; set; }
     public long StateId { get; set; }
 
